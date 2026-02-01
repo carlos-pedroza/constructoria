d5e2fd6e0d416806a5423bfb92f259622fcedab9
carlos pedroza <carlos.pedroza@capetisoft.com>
2026-01-31 20:57:19 -0600

    Production build

--- publish/flutter_service_worker.js
+++ publish/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5c424a434ea759967e22735f2b7039c8",
-"version.json": "7ade53385a5da1deeb8863ef32d6e485",
+const RESOURCES = {"flutter_bootstrap.js": "b0c83ad5984ec75430abf8c92481a6ee",
+"version.json": "8a2bb789ff63c605db3b8a622e408a6e",
 "splash/img/light-2x.png": "46b263757f349852abc6b2b945630c01",
 "splash/img/dark-4x.png": "0277c765c053ab255d30b241ebfe8a3c",
 "splash/img/light-3x.png": "e26c5df778b4de132d648ea8d1a51d73",
